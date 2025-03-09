692738429d0c005eda3d41149b541176a1829abb
ivanaguilar <ivanaguilar@gmail.com>
2025-03-09 17:31:30 +0100

    Processing in 15 batchs

--- reporte_modelos_concurrente.py
+++ reporte_modelos_concurrente.py
@@ -49,54 +49,54 @@ def deleteThink(texto):
 
 async def main():
     
+    for i in range(0, len(df), 30):
 
-    
+        batch_tweets = df['tweet'].iloc[i:i+30]
 
-    
-    for tweet in df['tweet']:
+        for tweet in batch_tweets:
 
-    
-                    #     "llama3.3",
-                    # "llama3.2",
-                    # "llama3.1",
-                    # "phi4",
-                    # "phi3",
-                    # "qwen2",
-                    # "qwen2.5"
-    
-        # message = f"""
-        # Clasify these messages in the following categories:
-        # 0 - hate speech 1 - offensive language 2 - neither
-        # IMPORTANT!!!: Put only the number to express the category.
-    
-        # {tweet}
-        # """
-
-        message = f"""
-        Classify the following message into one of these categories:
-        0 - Hate Speech
-        1 - Offensive Language
-        2 - Neither
         
-        IMPORTANT:
-        - Respond **only** with a single number (0, 1, or 2).
-        - Do **not** include any explanations or additional text.
-        - If the message is unclear, choose the closest matching category.
-    
-        Tweet: {tweet}
-        """
-    
+                        #     "llama3.3",
+                        # "llama3.2",
+                        # "llama3.1",
+                        # "phi4",
+                        # "phi3",
+                        # "qwen2",
+                        # "qwen2.5"
+        
+            # message = f"""
+            # Clasify these messages in the following categories:
+            # 0 - hate speech 1 - offensive language 2 - neither
+            # IMPORTANT!!!: Put only the number to express the category.
+        
+            # {tweet}
+            # """
+
+            message = f"""
+            Classify the following message into one of these categories:
+            0 - Hate Speech
+            1 - Offensive Language
+            2 - Neither
+            
+            IMPORTANT:
+            - Respond **only** with a single number (0, 1, or 2).
+            - Do **not** include any explanations or additional text.
+            - If the message is unclear, choose the closest matching category.
+        
+            Tweet: {tweet}
+            """
+        
+            for model in models:
+                # Procesar las líneas JSON y extraer texto
+        
+                model_predictions[model].append(enviar_prompt(message, model))
+        
         for model in models:
-            # Procesar las líneas JSON y extraer texto
-    
-            model_predictions[model].append(enviar_prompt(message, model))
-    
-    for model in models:
-    # Sustituimos el array de llamadas por un array de resultados para cada array de peticiones a modelos. Ya que la operación devuelve un array respuestas
-        model_predictions[model] = await asyncio.gather(*model_predictions[model])
-    
-    for model in models:
-        model_predictions[model] = [deleteThink(text) for text in model_predictions[model]]
+        # Sustituimos el array de llamadas por un array de resultados para cada array de peticiones a modelos. Ya que la operación devuelve un array respuestas
+            model_predictions[model] = await asyncio.gather(*model_predictions[model])
+        
+        for model in models:
+            model_predictions[model] = [deleteThink(text) for text in model_predictions[model]]
     
 
 
